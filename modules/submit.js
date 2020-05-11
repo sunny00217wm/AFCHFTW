cf0995597f91867093b691037d35324bb6bbeb80
sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
2020-05-11 21:55:55 +0800

    Update submit.js

--- modules/submit.js
+++ modules/submit.js
@@ -1,21 +1,53 @@
 //<nowiki>
-AFCH.submit = function submit() {
+AFCH.submit = function () {
 	if (!AFCH.test('submit')) {
 		return;
 	}
 
-	AFCH.addPortletLink(AFCH.submit.callback, AFCH.NameOfModule('submit'), 'afch-clear', wgULS('快速提交草稿', '快速提交草稿'));
+	AFCH.addPortletLink(AFCH.submit.callback, AFCH.NameOfModule('submit'), 'afch-submit', wgULS('快速提交草稿', '快速提交草稿'));
 };
  
 AFCH.submit.callback = function (){
-	///改start
 	var dialog;
 	AFCH.clear.dialog = new AFCH.SimpleWindow(AFCH.windowclass('clear'));
 	dialog = AFCH.clear.dialog;
-	dialog.setTitle(wgULS('进行维护清理', '進行維護清理'));
+	dialog.setTitle(wgULS('快速提交草稿', '快速提交草稿'));
 	dialog.setScriptName('AFCH');
 	dialog.addFooterLink('AFCH', 'Wikipedia:建立條目專題/協助腳本');
-	var form = new AFCH.Morebits.quickForm();
+	var form = new AFCH.Morebits.quickForm(AFCH.submit.evaluate);
+	
+	form.append({ type: 'radio', name: 'submituser',
+		list: [
+			{
+				label: wgULS('提交人：自己', '提交人：自己'),
+				value: 'me'
+			},
+			{
+				label: wgULS('提交人：页面创建者', '提交人：頁面建立者'),
+				value: 'create'
+			},
+			{
+				label: wgULS('提交人：其他用户', '提交人：其他用戶', '提交人：其他用户', '提交人：其他使用者'),
+				value: 'other'
+			}
+		],
+		event: AFCH.submit.changeTarget
+	});
+
+	form.append({
+		type: 'field',
+		label: '工作区',
+		name: 'work_area'
+	});
+
+	var result = form.render();
+	dialog.setContent(result);
+	dialog.display();
+	
+	var evt = document.createEvent('Event');
+	evt.initEvent('change', true, true);
+	result.tbtarget[0].dispatchEvent(evt);
+	/**
   	var result = form.render();
 	var pageobj = AFCH.Wikipedia.api;
   
@@ -37,5 +69,50 @@ AFCH.submit.callback = function (){
 	} else {
 		statelem.error(wgULS('抓取页面内容失败。', '抓取頁面內容失敗。'));
 	}
-}
+	**/
+};
+
+var submit_user = '';
+
+AFCH.submit.changeTarget = function(e) {
+	var value = e.target.values;
+	var root = e.target.form;
+	var old_area = AFCH.Morebits.quickForm.getElements(root, 'work_area')[0];
+
+	if (root.submit_user) {
+		submit_user = root.submit_user.value;
+	}
+
+	var work_area = new AFCH.Morebits.quickForm.element({
+		type: 'field',
+		label: wgULS('提交人信息', '提交人資訊'),
+		name: 'work_area'
+	});
+
+	root.previewer.closePreview();
+
+	if (value = 'other') {
+		work_area.append({
+			type: 'div',
+			label: '',
+			style: 'color: red',
+			id: 'afch-submit-optout-message'
+		});
+		work_area.append({
+			type: 'input',
+			name: 'submit_user',
+			label: wgULS('用户（必填）', '用戶（必填）', '用户（必填）', '使用者（必填）'),
+			tooltip: wgULS('您代為提交的用户名，必填。', '您代為提交的用戶名，必填。', '您代為提交的用户名，必填。', '您代為提交的使用者名稱，必填。'),
+			value: submit_user,
+			required: true
+		});
+	}
+
+	work_area = work_area.render();
+	root.replaceChild(work_area, old_area);
+
+	$('#afch-submit-optout-message').text(AFCH.submit.optout);
+};
+//https://github.com/Xi-Plus/twinkle/blob/master/modules/friendlytalkback.js#L293 +
+
 // </nowiki>
